92cabe2501ed3b2587b14804279cb5e015db3f5e
AwesomeHotdog24 <simonisaacfabry@gmail.com>
2022-05-08 07:42:35 -0400

    Setup scripts

---
+++ Card/Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    public bool follow;
+
+        
+    
+
+    void Update()
+    {
+       if(follow == true)
+       {
+            Follow();
+       }
+
+    }
+    void Follow()
+    {
+        
+    }
+}
---
+++ Card/Assets/Scripts/Varibles.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Varibles : MonoBehaviour {
+    public Vector3 mousepos;
+    public Camera MainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+}
